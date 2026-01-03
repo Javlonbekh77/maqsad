43a260cf9995cdd8742ab775385a68f1652391db
Javlonbek Xoliqulov <javlonbekh2007@gmail.com>
2026-01-03 13:05:35 +0000

    Interface theme shu yozuvni olib tashla

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -8,15 +8,14 @@ export default function ThemeSwitcher() {
   const { theme, setTheme } = useTheme()
 
   return (
-    <div className="flex items-center space-x-2">
-      <p className="text-sm text-muted-foreground">Interface theme</p>
+    <div className="flex items-center">
       <Button
-        variant="outline"
+        variant="ghost"
         size="icon"
         onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
       >
-        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         <span className="sr-only">Toggle theme</span>
       </Button>
     </div>
